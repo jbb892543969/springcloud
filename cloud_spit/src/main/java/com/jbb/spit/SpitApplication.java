5bbaba0d5493e8b4d80e86977868a80639158a6b
金斌斌 <892543969@qq.com>
2019-03-18 09:30:29 +0800

    吐槽模块(mongodb)

---
+++ cloud_spit/src/main/java/com/jbb/spit/SpitApplication.java
@@ -0,0 +1,17 @@
+package com.jbb.spit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author jbb
+ * @date 2019/3/18
+ */
+@SpringBootApplication
+public class SpitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpitApplication.class);
+    }
+}
